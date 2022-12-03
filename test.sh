e48a50d04ddbd411642302be145654d31f554e02
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-04 01:51:47 +0530

    updated

--- test.sh
+++ test.sh
@@ -1,2 +1,1 @@
-aws ecr describe-images --repository-name ecs_repo \
---query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' jq . --raw-output
+aws ecr describe-images --repository-name ecs_repo --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' --output text
