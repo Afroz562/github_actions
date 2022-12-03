46493336f453254539ca5067c4d295b563f866fc
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-04 02:18:55 +0530

    latest

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #aws ecr describe-images --repository-name ecs_repo --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output text  \
-aws ecr describe-images --repository-name ecs-repo \
+aws ecr describe-images --repository-name ecs_repo \
 --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
 | tail -n 3 | awk -F'- ' '{print $2}' | head -2
