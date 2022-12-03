6728a54df42d2a3cb3321d0de0efb976e5786312
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-04 01:14:23 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
+mkdir test
+cd test
 aws ecr describe-images --repository-name gvh \
 --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
 | tail -n 3 | awk -F'- ' '{print $2}' >> output.txt
+cat output.txt
