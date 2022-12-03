19b033a616a45afa3ff487676a355f97a53b46bf
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-04 02:22:34 +0530

    latest

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #aws ecr describe-images --repository-name ecs_repo --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output text  \
 aws ecr describe-images --repository-name ecs_repo \
---query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
-| tail -n 3 | awk -F'- ' '{print $2}' | head -2 \ --output text 
+--query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output text \ --output yaml \
+| tail -n 3 | awk -F'- ' '{print $2}' | head -2  
