1b2c51f639faf603f94a5ddae00a577f3f84415d
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-04 02:08:40 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-aws ecr describe-images --repository-name ecs_repo --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' --output text
+aws ecr describe-images --repository-name ecs_repo --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output text  \
+| tail -n 3 | awk -F'- ' '{print $2}' >> $GITHUB_OUTPUT
