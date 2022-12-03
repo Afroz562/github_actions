f4d4da0576e730f60101cd18b699b39e08ba5719
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-04 02:11:26 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 aws ecr describe-images --repository-name ecs_repo --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output text  \
-| tail -n 3 | awk -F'- ' '{print $2}' >> $GITHUB_OUTPUT
+
