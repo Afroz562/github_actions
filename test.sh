4fe16bcc6ba81248153fa95b98dc3e7a3a77a891
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-04 20:54:54 +0530

    Rollback

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #aws ecr describe-images --repository-name ecs_repo --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output text 
 #!/bin/bash
 #BUILD_VERSION="$1"
- aws ecr describe-images --repository-name ecs_repo \
---query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
-| tail -n 2 | awk -F'- ' '{print $2}' | head -1 | >> $GITHUB_ENV
+
+# aws ecr describe-images --repository-name ecs_repo \
+#--query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
+#| tail -n 2 | awk -F'- ' '{print $2}' | head -1  >> $GITHUB_ENV
 #echo ‘EOF’ >> $GITHUB_ENV
 #echo "$BUILD_VERSION"
 #>> env.$GITHUB_OUTPUT
@@ -11,3 +12,4 @@
 #--query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
 #|tail -n 3 |awk -F'- ' '{print $2}' |head -1`
 #echo $"AWS_REPOS" 
+aws ecr describe-images --output json --repository-name ecs_repo $DOCKER_IMAGE_NAME --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' | jq . --raw-output
