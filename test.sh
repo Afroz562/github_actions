aa41a2ec148444524a08e181e572b1946aa2961e
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-04 13:24:44 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #aws ecr describe-images --repository-name ecs_repo --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output text 
 aws ecr describe-images --repository-name ecs_repo \
 --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
-| tail -n 3 | awk -F'- ' '{print $2}' 
+| tail -n 3 | awk -F'- ' '{print $2}' | head -2
 #>> env.$GITHUB_OUTPUT
