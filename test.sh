bb1fb71908e75bbe703f9becab238cd31afbffe5
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-04 20:17:24 +0530

    rollback

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #aws ecr describe-images --repository-name ecs_repo --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output text 
 #!/bin/bash
 #BUILD_VERSION="$1"
-echo `aws ecr describe-images --repository-name ecs_repo \
+ aws ecr describe-images --repository-name ecs_repo \
 --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
-| tail -n 2 | awk -F'- ' '{print $2}' | head -1 | grep -v “::” | sed -e ‘s/"//g’` >> $GITHUB_ENV
+| tail -n 2 | awk -F'- ' '{print $2}' | head -1 | grep -v “::” | sed -e ‘s/"//g’ >> $GITHUB_ENV
+echo ‘EOF’ >> $GITHUB_ENV
 #echo "$BUILD_VERSION"
 #>> env.$GITHUB_OUTPUT
 #AWS_REPOS=`aws ecr describe-images --repository-name ecs_repo\
