22f481aae5405f5d4684437151430247e999db40
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-04 14:07:02 +0530

    Latest

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 #VAR1="$1"
  aws ecr describe-images --repository-name ecs_repo \
 --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
-| tail -n 3 | awk -F'- ' '{print $2}' | head -1` >> $BUILD_VERSION
+| tail -n 3 | awk -F'- ' '{print $2}' | head -1  >> $BUILD_VERSION
 echo "$BUILD_VERSION"
 #>> env.$GITHUB_OUTPUT
