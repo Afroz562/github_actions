d1f2c6f41c828b269832d85db82a555333af09b6
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-04 22:29:49 +0530

    rollback

--- test.sh
+++ test.sh
@@ -17,8 +17,9 @@
 #!/bin/bash
 
 #result=$(aws ecr describe-images --output json --repository-name ecs_repo $DOCKER_IMAGE_NAME --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' | jq . --raw-output)
-result=$( aws ecr describe-images --repository-name ecs_repo \ --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \ 
-| tail -n 2 | awk -F'- ' '{print $2}' | head -1)
+result=$( aws ecr describe-images --repository-name ecs_repo \
+--query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
+| tail -n 3 | awk -F'- ' '{print $2}' | head -1)
 for val in $result; do
     echo "Run: Display IMAGE TAG $val"
     #echo "Run:build_id=$val"
