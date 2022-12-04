7eed018da25e806941eb9d59e6b3cd6ce81ec5bd
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-04 22:32:41 +0530

    Rollback

--- test.sh
+++ test.sh
@@ -20,7 +20,7 @@ result=$( aws ecr describe-images --repository-name ecs_repo \
 | tail -n 3 | awk -F'- ' '{print $2}' | head -2)
 for val in $result; do
     echo "Run: Display IMAGE TAGS $val"
-
+done
 #result=$(aws ecr describe-images --output json --repository-name ecs_repo $DOCKER_IMAGE_NAME --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' | jq . --raw-output)
 result=$( aws ecr describe-images --repository-name ecs_repo \
 --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
