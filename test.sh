609098f932de2eaab3b4f147d224e598861804fd
Afroz562 <70379944+Afroz562@users.noreply.github.com>
2022-12-05 11:05:17 +0530

    Latest

--- test.sh
+++ test.sh
@@ -17,17 +17,17 @@
 #!/bin/bash
 result=$( aws ecr describe-images --repository-name ecs_repo \
 --query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
-| tail -n 10 )
+| tail -n 3  | awk -F'- ' '{print $2}')
 for val in $result; do
     echo "Run: Display IMAGE TAGS $val"
 done
-#result=$(aws ecr describe-images --output json --repository-name ecs_repo $DOCKER_IMAGE_NAME --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' | jq . --raw-output)
-#result=$( aws ecr describe-images --repository-name ecs_repo \
-#--query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
-#| tail -n 3 | awk -F'- ' '{print $2}' | head -1)
-#for val in $result; do
-   # echo "Run: Display IMAGE TAG $val"
+result=$(aws ecr describe-images --output json --repository-name ecs_repo $DOCKER_IMAGE_NAME --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' | jq . --raw-output)
+result=$( aws ecr describe-images --repository-name ecs_repo \
+--query 'sort_by(imageDetails,& imagePushedAt)[*].imageTags[0]' --output yaml \
+| tail -n 3 | awk -F'- ' '{print $2}' | head -1)
+for val in $result; do
+    echo "Run: Display IMAGE TAG $val"
     #echo "Run:build_id=$val"
     #echo ::set-output {name}=image::$val"
     #echo "::set-output name=image::056584266475.dkr.ecr.us-east-1.amazonaws.com/ecs_repo:$val"
-#done
+done
